6396150e9ac8072dd3da0a1d9a0012903606115d
uonai <cs.mccaleb@gmail.com>
2020-08-14 19:48:38 -0500

    Messages, miners, api limit for pagination

--- index.js
+++ index.js
@@ -70,6 +70,90 @@ app.get("/block/:cid", async (req, res) => {
     console.error(error);
   }
 });
+
+// MESSAGES
+const Message = sequelize.define(
+  "messages",
+  {
+    cid: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    from: {
+      type: Sequelize.STRING,
+    },
+    to: {
+      type: Sequelize.STRING,
+    },
+    nonce: {
+      type: Sequelize.STRING,
+    },
+    gasprice: {
+      type: Sequelize.NUMBER,
+    },
+    gaslimit: {
+      type: Sequelize.NUMBER,
+    },
+  },
+  {
+    // options
+  }
+);
+app.get("/messages", async (req, res) => {
+  // const blockId = req.params.cid;
+  // console.log(blockId);
+  try {
+    const message = await Message.findAll({
+      attributes: ["cid", "from", "to", "nonce", "gasprice", "gaslimit"],
+      offset: 10,
+      limit: 10,
+    });
+    res.json({ message });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// MINER POWER
+const MinerPower = sequelize.define(
+  "miner_powers",
+  {
+    miner_id: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    state_root: {
+      type: Sequelize.STRING,
+    },
+    raw_bytes_power: {
+      type: Sequelize.NUMBER,
+    },
+    quality_adjusted_power: {
+      type: Sequelize.NUMBER,
+    },
+  },
+  {
+    // options
+  }
+);
+app.get("/minerpowers", async (req, res) => {
+  // const blockId = req.params.cid;
+  // console.log(blockId);
+  try {
+    const minerpower = await MinerPower.findAll({
+      attributes: [
+        "miner_id",
+        "state_root",
+        "raw_bytes_power",
+        "quality_adjusted_power",
+      ],
+    });
+    res.json({ minerpower });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 app.listen(port, () =>
   console.log(`Slate stats api listening on port ${port}!`)
 );
